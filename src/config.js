43598b259f97d7291f324107b08efd6651773588
manol-sh <168610897+manol-sh@users.noreply.github.com>
2024-10-16 12:22:59 -0500

    adding a random api key to prevent build failure

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: Replace next line with your own public API key
-  builderAPIKey: '',
+  builderAPIKey: '59bb518773c14842921abe05d5e2bee3',
 };
