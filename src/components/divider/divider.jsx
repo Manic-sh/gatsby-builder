4a4012f4fc40272ee7068019b25ec8a8d328442f
manish-builder-io <manish@builder.io>
2024-10-29 23:05:46 +0530

    refactor custom component

--- src/components/divider/divider.jsx
+++ src/components/divider/divider.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import builder, { Builder } from "@builder.io/react";
 import './divider.styles.css';
+
+builder.init("0f9e8947659247f9a7ae2506f4375e34");
 
 function Divider({ text = 'This is a Custom Divider Component' }) {
   return (
@@ -10,3 +13,16 @@ function Divider({ text = 'This is a Custom Divider Component' }) {
 }
 
 export default Divider;
+
+Builder.registerComponent(Divider, {
+  name: "Custom Divider",
+  inputs: [
+    {
+      name: "text",
+      type: "string",
+      defaultValue: "This is a Custom Divider Component",
+      required: true,
+      showIf: "true",
+    },
+  ],
+});
